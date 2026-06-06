44f51656003c4c23fee0cc132f5940d06ed411ff
AlexAaqil <alexaaqil.se@gmail.com>
2026-06-07 02:22:32 +0300

    brand strategy content updates.

--- src/data/app.config.ts
+++ src/data/app.config.ts
@@ -1,3 +1,10 @@
+import businessOwner from '../assets/images/business-owner.jpeg';
+import womanFounder from '../assets/images/woman-founder.jpeg';
+
 export const AppConfig = {
   brand_name: 'Aswa',
+  images: {
+    business_owner: businessOwner,
+    woman_founder: womanFounder
+  }
 } as const;
